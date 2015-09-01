60b425cfb4b83662badfc3bee9910fcb1b02b517
inei <iancnei@gmail.com>
2015-08-31 17:13:01 -0700

    create ability to add, display, and delete comments without refershing

--- index.js
+++ index.js
@@ -29,6 +29,50 @@ app.get("/books", function (req, res) {
         });
 });
 
+app.post("/books", function(req, res)
+{
+    //console.log(req.body);
+    db.Book.findOne({_id: req.body._id}, function (err, book) {
+        if(err) {return console.log(err);}
+        var newComment = {
+            comment: req.body.comment,
+            person: req.body.person
+        };
+        book.comments.push(newComment);
+        book.save(function(err, success) {
+            if(err) {return console.log(err);}
+            console.log("Comment added Successfully");
+            res.send(newComment);
+        });
+    })
+
+});
+
+app.delete("/books", function(req, res)
+{
+    var ids = req.body._id.toString().split("=");
+    db.Book.findOne({_id: ids[0]}, function (err, book) {
+        if(err) {return console.log(err);}
+        /* To remove ANY embedded document, simplly call .remove(); */
+        //book.comments[0].remove();
+        for(var i = 0; i < book.comments.length; i++)
+        {
+            if(book.comments[i]._id.toString() === ids[1])
+            {
+                //console.log("match found");
+                book.comments[i].remove();
+                break;
+            }
+        }
+        //console.log(book.comments);
+        book.save(function(err, success) {
+            if(err) {return console.log(err);}
+            res.send(success);
+        });
+    })
+
+});
+
 app.listen(3000, function() {
     console.log("Server is now listening on localhost:3000");
 })
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,20 +1,58 @@
-// wait for the document to be ready
-$(function () {
-    console.log("app.js is now running");
+function sendComment(context)
+{
+    var id = context.id;
+    var data = $("#" + id + "form").serialize();
+    data += "&_id=" + id;
+    console.log(data);
+    id = "#" + context.id;
+    $.post("/books", data)
+    .done(
+        function(response)
+        {
+            renderBooks();
+            $(id + "form")[0].reset();
+        }
+    );
+    return false;
+}
 
+function deleteComment(context)
+{
+    var commentId = context.id;
+    //console.log(commentId);
+    $.ajax({
+        url: '/books',
+        type: 'DELETE',
+        data: {_id: commentId},
+        success: function(res)
+        {
+            //console.log("deletion successful");
+            renderBooks();
+        }
+    });
+}
+
+function renderBooks()
+{
     var $booksCon = $("#booksCon");
     var bookHTML = $("#bookTemp").html();
     var bookTemp = _.template(bookHTML);
 
+    $booksCon.empty();
+
     $.get("/books").
         done(function(data) {
-            console.log(data);
+            //console.log(data);
             $(data).each(function (index, book) {
                 var $book = $(bookTemp(book));
                 $booksCon.append($book);
             });
     });
+}
 
+// wait for the document to be ready
+$(function () {
+    console.log("app.js is now running");
 
-
+    renderBooks();
 });
